66537a760c843a5b5728ecfdf341d072350c1cd7
Louise Hsu <louiseh0313@yahoo.ca>
2018-02-26 11:36:20 -0800

    Initial commit

---
+++ src/gov/nasa/worldwindx/examples/PlacemarkParser.java
@@ -0,0 +1,15 @@
+package gov.nasa.worldwindx.examples;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class PlacemarkParser
+{
+    public List<Placemarks> parsePlacemarks(String s) throws JSONException {
+        return null;
+    }
+
+}
